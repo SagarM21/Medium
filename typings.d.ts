a4cb415d567923af9e19c15d97fbf7ffc440c965
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-20 19:32:09 +0530

    Comments Functionality done

--- typings.d.ts
+++ typings.d.ts
@@ -4,6 +4,7 @@ export interface Post {
 	_createdAt: string;
 	title: string;
 	description: string;
+	comments: Comment[];
 	author: {
 		name: string;
 		image: string;
@@ -17,4 +18,20 @@ export interface Post {
 		current: string;
 	};
 	body: [object];
+}
+
+export interface Comment {
+	approved: boolean;
+	comment: string;
+	email: string;
+	name: string;
+	post: {
+		_ref: string;
+		_type: string;
+	};
+	_createdAt: string;
+	_id: string;
+	_rev: string;
+	_type: string;
+	_updatedAt: string;
 }
